339414a01a4984e5102ed54e492fa795069ab75b
AlbertNoMes <albertnotariomestres@gmail.com>
2022-01-19 11:10:23 +0100

    Afegit sysos finals

--- src/Raices.java
+++ src/Raices.java
@@ -1,12 +1,10 @@
-
 public class Raices {
 
-	//Atributos(caracteristicas)
+	//Atributos(valores)
 	private double a;
 	private double b;
 	private double c;
 	
-
 	//Constructores
 	public Raices(double a, double b, double c) {
 		this.a = a;
@@ -14,17 +12,8 @@ public class Raices {
 		this.c = c;
 	}
 	
-
 	//Metodos
-	//getDiscriminante
-	public double getDiscriminante() {
-			
-		double resultado = Math.pow(b, 2)-(4*a*c);
-		return resultado;
-			
-	}
-	
-	
+
 	//ObtenerRaices
 	public void obtenerRaices() {
 		
@@ -32,29 +21,32 @@ public class Raices {
 		double resultado2 = (-b-Math.sqrt(Math.pow(b, 2)-(4*a*c)))/(2*a);
 		
 		System.out.println("Los resultados de la operación són: "+resultado1+" y "+resultado2);
-		
 	}
 	
 	//ObtenerRaiz
 	private void obtenerRaiz(){
         
         double x=-b+Math.sqrt(0)/(2*a);
-  
-        System.out.println("El resultado de esta operación es: "+x);
-         
+        
+        System.out.println("El resultado de esta operación es: "+x);       
     }
+	
+	//getDiscriminante
+	public double getDiscriminante() {
+			
+		double resultado = Math.pow(b, 2)-(4*a*c);
+		return resultado;	
+	}
 	
 	//tineneRaices
 	private boolean tieneRaices(){
         return getDiscriminante()>0;
     }
 	
-	
 	//tieneRaiz
     private boolean tieneRaiz(){
         return getDiscriminante()==0;
     }
-	
 	
 	//calcular
     public void calcular(){
@@ -66,45 +58,30 @@ public class Raices {
         }else{
             System.out.println("Esta operacion no tiene soluciones");
         }
-         
     }
-
+    
 	//getters & setters
-
 	public double getA() {
 		return a;
 	}
-
-
 
 	public void setA(double a) {
 		this.a = a;
 	}
 
-
-
 	public double getB() {
 		return b;
 	}
-
-
 
 	public void setB(double b) {
 		this.b = b;
 	}
 
-
-
 	public double getC() {
 		return c;
 	}
 
-
-
 	public void setC(double c) {
 		this.c = c;
 	}
-	
-	
-	
 }
--- src/RaicesApp.java
+++ src/RaicesApp.java
@@ -6,11 +6,9 @@ public class RaicesApp {
 		Raices r1 = new Raices(1, 2, 3);
 		Raices r2 = new Raices(31, 145, 36);
 		
+		System.out.println("La operación 1 tiene como resultado: ");
 		r1.calcular();
+		System.out.println("La operación 2 tiene como resultado:");
 		r2.calcular();
-		
-		
-		
 	}
-
 }
